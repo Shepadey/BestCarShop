356772c00430e35c0aeca28669b623a034fe7422
Иван Иванов <ivanivanov@MacBook-Pro-Ivan.local>
2022-08-21 13:01:16 +0300

    new

--- core/static/js/index.js
+++ core/static/js/index.js
@@ -16,6 +16,7 @@ axios.get(url,{
     console.log(response);
     })
 })
+
 let inputSearch = document.getElementById( 'inputWord');
 console.log(inputSearch)
 inputSearch.addEventListener('input', function(event) {
@@ -23,12 +24,36 @@ inputSearch.addEventListener('input', function(event) {
 
     let inputValue = document.getElementById( 'search' ).value
 
-    axios.get('http://127.0.0.1:800/search-item/',{
+axios.get('http://127.0.0.1:800/search-item/',{
         params:{
         searchWord:inputValue
         }
-    })
-    .then(function(response) {
-    console.log(response)
-    })
-}).then(function)
+}).then(function(response) {
+    let allItems=document.querySelectorAll('.item')
+    allItems.forEach(item=>item.remove())
+
+    let data = response.data
+
+    for(let index in data){
+        let div = document.createElement('div')
+        let h4 = document.createElement('h4')
+        let p = document.createElement('p')
+        let h3 = document.createElement('h3')
+        let img = document.createElement('img')
+
+        img.setAttribute('src', data[index].image)
+
+        div.classList.add('item')
+        h4.innerText = data[index].name
+        p.innerText = data[index].color
+        h3.innerText = '${data[index].cost}'
+
+        div.appendChild(img)
+        div.appendChild(h4)
+        div.appendChild(p)
+        div.appendChild(h3)
+
+        document.querySelector('.items').appendChild(div)
+    }
+})
+})
--- core/views.py
+++ core/views.py
@@ -5,35 +5,7 @@ from core.models import Example
 from urllib import request
 from django.shortcuts import redirect, render
 from django.db.models import  Max, Min
-def main (request): 
-    if request.method =='POST':
-            from_number = request.POST.get('first')
-            to_number = request.POST.get('second')
-            items = Example.objects.filter(cost__range=(from_number, to_number))
-            return render(request,'main.html',{"items":items})
-    else:
-        items = Example.objects.all()
-        min_cost = Example.objects.all().aggregate(Min('cost'))
-        max_cost = Example.objects.all().aggregate(Max('cost'))
-        return render(request,'main.html',{"items":items,"min_cost":min_cost,"max_cost":max_cost})
-def form(request):
-    forms =ExampleForm()
-    if request.method == "POST":
-        form = ExampleForm(request.POST, request.FILES)
-        if form.is_valid():
-            form.save()
-            return redirect('main')
-    return render(request,'add.html',{'forms':forms})
-def get_items(request):
-    return JsonResponse({'info':'Фильтр работает'})
-
-def search_items(request):
-    print(request.GET)
-    return JsonResponse({'info':'Поиск работает'})
-
-def search_item(request):
-    searchWord = str(request.GET.get("inputWord"))
-    items = ShopItem.object.filter(name__container=searchWord) 
+def create_dict_with_items(items):
     response = {}
     for item in items:
         response[item.id] = {
@@ -43,8 +15,43 @@ def search_item(request):
             'cost' : item.cost,
             'image' : item.image
         }
-    print(response)
-    return JsonResponse(response)
+def main (request): 
+    '''if request.method =='get':
+            from_number = request.GET.get('first')
+            to_number = request.GET.get('second')
+            items = Example.objects.filter(cost__range=(from_number, to_number))
+            return render(request,'main.html',{"items":items})'''
+    items = Example.objects.all()
+    min_cost = Example.objects.all().aggregate(Min('cost'))
+    max_cost = Example.objects.all().aggregate(Max('cost'))
+    print(min_cost)
+    print(max_cost)
+    return render(request,'main.html',{"items":items,"min_cost":min_cost,"max_cost":max_cost})
+def form(request):
+    forms =ExampleForm()
+    if request.method == "POST":
+        form = ExampleForm(request.POST, request.FILES)
+        if form.is_valid():
+            form.save()
+            return redirect('main')
+    return render(request,'add.html',{'forms':forms})
+def get_items(request):
+    from_number = request.GET.get('from')
+    to_number = request.GET.get('to')
+
+    items = ShopItem.objects.filter(price__range=(from_number, to_number))
+
+    return JsonResponse(create_dict_with_items(items))
+
+
+def search_items(request):
+    print(request.GET)
+    return JsonResponse({'info':'Поиск работает'})
+
+def search_item(request):
+    searchWord = str(request.GET.get("inputWord"))
+    items = Example.object.filter(name__container=searchWord) 
+    return JsonResponse(create_dict_with_items(items))
 
     
 
